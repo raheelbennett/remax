e365f47034a5b408c818887d3f475608af9ce225
lian-c <lian.chan@telus.com>
2023-02-03 11:16:05 -0800

    Add categories seed data

---
+++ config/schema/seed.sql
@@ -0,0 +1,15 @@
+INSERT INTO categories (name)
+VALUES
+('Grocery'),
+('Recurring Bill'),
+('Furniture'),
+('Drug Store'),
+('Restaurants'),
+('Home Improvement'),
+('Hotel'),
+('Entertainment'),
+('Gas'),
+('Transit'),
+('Other')
+
+#https://www.tangerine.ca/en/products/spending/creditcard/money-back categories from here
