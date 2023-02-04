05de27d9e79aa97746e8c67b866f57df101c693d
Aingaran Thirunadarajah <email@aingaran.com>
2023-02-04 03:55:15 -0500

    Changed order of home page listing.. Moved cards before vendors

--- pages/index.js
+++ pages/index.js
@@ -1,109 +1,98 @@
-import Link from 'next/link'
-import CardItem from '../components/cards/CardItem'
-import VendorItem from '../components/vendors/VendorItem'
-import CardsList from '../components/cards/CardsList'
-
+import Link from "next/link";
+import CardItem from "../components/cards/CardItem";
+import VendorItem from "../components/vendors/VendorItem";
+import CardsList from "../components/cards/CardsList";
 
 const vendors = [
   {
     id: 1,
     name: "Home Depot",
     image: "assets/img/vendor/home-depot.png",
-    link: "/vendor/home-depot"
+    link: "/vendor/home-depot",
   },
   {
     id: 2,
     name: "Amazon",
     image: "assets/img/vendor/amazon.png",
-    link: "/vendor/amazon"
+    link: "/vendor/amazon",
   },
   {
     id: 3,
     name: "Telus",
     image: "assets/img/vendor/telus.png",
-    link: "/vendor/telus"
+    link: "/vendor/telus",
   },
   {
     id: 4,
     name: "Boston Pizza",
     image: "assets/img/vendor/boston-pizza.png",
-    link: "/vendor/boston-pizza"
+    link: "/vendor/boston-pizza",
   },
   {
     id: 5,
 
     name: "Netflix",
     image: "assets/img/vendor/netflix.png",
-    link: "/vendor/netflix"
+    link: "/vendor/netflix",
   },
   {
     id: 6,
     name: "Rogers",
     image: "assets/img/vendor/rogers.png",
-    link: "/vendor/rogers"
+    link: "/vendor/rogers",
   },
 
   {
     id: 7,
     name: "The Keg",
     image: "assets/img/vendor/the-keg.jpeg",
-    link: "/vendor/the-keg"
+    link: "/vendor/the-keg",
   },
 
   {
     id: 8,
     name: "McDonalds",
     image: "assets/img/vendor/mcondalds.png",
-    link: "/vendor/mcdonalds"
+    link: "/vendor/mcdonalds",
   },
 
   {
     id: 9,
     name: "Moxies",
     image: "assets/img/vendor/moxies.jpeg",
-    link: "/vendor/moxies"
+    link: "/vendor/moxies",
   },
   {
     id: 10,
     name: "Disney Plus",
     image: "assets/img/vendor/disney-plus.jpeg",
-    link: "/vendor/disney-plus"
-  }
-]
+    link: "/vendor/disney-plus",
+  },
+];
 
 const home = () => {
-
   return (
-      <main>
+    <main>
+      <section className="main-content">
 
-
-        <section className="main-content">
-          <h2>Vendors</h2>
+      <h2>Cards</h2>
+        <CardsList />
 
         
-          <div className="store-list grid grid-cols-4 gap-4">
+        <h2>Vendors</h2>
 
-{
-  vendors.map((vendor) => (
-    <Link href={vendor.link}>
-    <VendorItem vendor={vendor} />
-    </Link>
-  ))  
-}
+        <div className="store-list grid grid-cols-4 gap-4">
+          {vendors.map((vendor) => (
+            <Link href={vendor.link}>
+              <VendorItem vendor={vendor} />
+            </Link>
+          ))}
+        </div>
 
-          </div>
+   
+      </section>
+    </main>
+  );
+};
 
-          <h2>Cards</h2>
-          <CardsList />
-          <div className="card-list grid grid-cols-3 gap-4">
-
-          </div>
-
-        </section>
-  
-      </main>
-
-  )
-}
-
-export default home
+export default home;
