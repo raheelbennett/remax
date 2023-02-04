00dfd460903d7b38ae82c0f683321a545528899e
lian-c <lian.chan@telus.com>
2023-02-04 10:43:53 -0800

    Update helper function

--- config/queries/helper.js
+++ config/queries/helper.js
@@ -5,13 +5,18 @@ const getBanks = () => {
     .then(data => data.rows)
     .catch(err => err.message);
 };
+const getCategories = () => {
+  return db.query('SELECT name FROM categories ORDER BY name;')
+    .then(data => data.rows)
+    .catch(err => err.message);
+};
 const getCategoriesByCardID = (id) => {
-  return db.query('SELECT categories.name as category, reward_rate as Cashback, cards.name as cards from rewards JOIN categories on category_id=categories.id JOIN cards on card_id=cards.id WHERE card_id = $1;',[id] )
+  return db.query('SELECT categories.name as category, reward_rate as Cashback, cards.name as cards FROM rewards JOIN categories on category_id=categories.id JOIN cards on card_id=cards.id WHERE card_id = $1;',[id] )
     .then(data => data.rows)
     .catch(err => err.message);
 };
-const getAllCategories = () => {
-  return db.query('SELECT categories.name as category, reward_rate as Cashback, cards.name as cards from rewards JOIN categories on category_id=categories.id JOIN cards on card_id=cards.id ORDER BY Cashback DESC')
+const getAllCashback = () => {
+  return db.query('SELECT categories.name as category, reward_rate as Cashback, cards.name as cards, cards.interest_rate as interest, cards.annual_fee as annual_fee from rewards JOIN categories on category_id=categories.id JOIN cards on card_id=cards.id ORDER BY Cashback DESC;')
     .then(data => data.rows)
     .catch(err => err.message);
 };
@@ -19,4 +24,4 @@ const getAllCategories = () => {
 
 
 
-module.exports = { getBanks, getCategoriesByCardID , getAllCategories};
+module.exports = { getBanks, getCategoriesByCardID , getAllCashback, getCategories};
