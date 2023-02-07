c73ac5ed4dd3a4efc8427b76c5c713d0ff6905e1
Raheel Bennett <raheel.bennett@telus.com>
2023-02-07 02:08:25 -0700

    add skelaton for collapsable sideNav bar

--- components/nav/SubNav.js
+++ components/nav/SubNav.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Sidebar } from "flowbite-react";
 
 const SubNav = () => {
   const [categories, setCategories] = useState([]);
@@ -13,16 +14,34 @@ const SubNav = () => {
   }, []);
 
   return (
-    <div className="sub-nav p-8 bg-slate-200 w-80 h-1/2 text-black rounded-md mx-2">
-      <ul className="flex flex-col">
-        {categories.map((category) => (
-          <li key={category.id} className="basis-20">
-            <Link href={`/categories/${category.id}`} className="font-sans font-semibold text-2xl">
-              {category.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="w-fit font-extrabold	">
+      <Sidebar aria-label="Sidebar with multi-level dropdown" className="bg-slate-200 ">
+        <Sidebar.Items className="sub-nav p-8  bg-slate-200 w-80 rounded">
+          <Sidebar.ItemGroup className="">
+            <Sidebar.Collapse className=" text-4xl" label="Categories">
+              {categories.map((category) => (
+                <Sidebar.Item key={category.id} href={`/categories/${category.id}`}>
+                  {category.name}
+                </Sidebar.Item>
+              ))}
+            </Sidebar.Collapse>
+            <Sidebar.Collapse className=" text-4xl" label="Vendors">
+              <Sidebar.Item href="#">Products</Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Collapse className=" text-4xl" label="Banks">
+              <Sidebar.Item href="#">Products</Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Collapse className=" text-4xl" label="Cards">
+              <Sidebar.Item href="#">Products</Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Item href="#">Inbox</Sidebar.Item>
+            <Sidebar.Item href="#">Users</Sidebar.Item>
+            <Sidebar.Item href="#">Products</Sidebar.Item>
+            <Sidebar.Item href="#">Sign In</Sidebar.Item>
+            <Sidebar.Item href="#">Sign Up</Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
     </div>
   );
 };
