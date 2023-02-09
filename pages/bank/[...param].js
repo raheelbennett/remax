d99bcdc92e93dc0f0c711285ea33d9d5e30c6d31
Raheel Bennett <raheel.bennett@telus.com>
2023-02-08 17:08:19 -0700

    add conditional to remedy error when there are no cards for a bank

--- pages/bank/[...param].js
+++ pages/bank/[...param].js
@@ -23,30 +23,34 @@ const Bank = ({ cards, cashback }) => {
     return <div>All</div>;
   }
   return (
-    <section className="w-1/2">
-      <h2 className="text-2xl font-bold m-8">{cards[0]["bank"]}</h2>
-      <div className="card-list grid grid-cols-1 gap-10 m-10">
-        {cards.map((card) => (
-          <div className="p-6 rounded-lg shadow-md" key={card.id}>
-            <Link href="/cards/[id]" as={`/cards/${card.id}`}>
-              <div className=" grid grid-cols-2">
-                <CardItem card={card} />
-                <div className="col-start-2 col-end-3 ">
-                  <h3 className="text-xl font-bold">{card.name}</h3>
-                  {rewardsArrayByCardId(card.id).map((reward) => (
-                    <div key={reward.name} className="text-sm font-extrabold text-gray-900">
-                      Cashback Rate for {reward.category}: {reward.cashback}%
+    cards.length > 0 && (
+      <section className="w-1/2">
+        <h2 className="text-2xl font-bold m-8">{cards[0]["bank"]}</h2>
+        <div className="card-list grid grid-cols-1 gap-10 m-10">
+          {cards.map((card) => (
+            <div className="p-6 rounded-lg shadow-md" key={card.id}>
+              <Link href="/cards/[id]" as={`/cards/${card.id}`}>
+                <div className=" grid grid-cols-2">
+                  <CardItem card={card} />
+                  <div className="col-start-2 col-end-3 ">
+                    <h3 className="text-xl font-bold">{card.name}</h3>
+                    {rewardsArrayByCardId(card.id).map((reward) => (
+                      <div key={reward.name} className="text-sm font-extrabold text-gray-900">
+                        Cashback Rate for {reward.category}: {reward.cashback}%
+                      </div>
+                    ))}
+                    <div className="text-sm text-gray-700">Annual Fee: ${card.annual_fee}</div>
+                    <div className="text-sm text-gray-500">
+                      Interest Rate: {card.interest_rate}%
                     </div>
-                  ))}
-                  <div className="text-sm text-gray-700">Annual Fee: ${card.annual_fee}</div>
-                  <div className="text-sm text-gray-500">Interest Rate: {card.interest_rate}%</div>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </section>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 export default Bank;
