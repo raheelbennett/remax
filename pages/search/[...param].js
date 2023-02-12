147119d4f6ec2f048daa9b60023d92aa58686cba
Raheel Bennett <raheel.bennett@telus.com>
2023-02-12 13:51:54 -0700

    add banks to search results

--- pages/search/[...param].js
+++ pages/search/[...param].js
@@ -53,7 +53,7 @@ export default function SearchResults({ banks, cards, vendors }) {
 
   return (
     <div className="">
-      <h1 className="text-3xl font-bold m-4">Search Results</h1>
+      <h1 className="text-3xl font-bold m-5">Search Results</h1>
 
       {cardResults()}
       {vendorResults()}
