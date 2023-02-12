4df779909e66e89de7a82a763e06a2d995bbffd9
Aingaran Thirunadarajah <email@aingaran.com>
2023-02-12 16:24:25 -0500

    added conditional display of bank logo

--- components/banks/BankList.jsx
+++ components/banks/BankList.jsx
@@ -7,15 +7,20 @@ const BankList = ({ banks }) => {
   return (
     <>
       {banks.map((bank) => (
+
+
+
         <div
           className="p-4 rounded-lg shadow-lg hover:bg-slate-100 hover:drop-shadow-xl flex flex-wrap flex-col content-around justify-around"
           key={bank.id}
+
         >
           <Link href={`/bank/${bank.id}`} scroll={false} onClick={handleClickScroll}>
             <div className="text-center">
               <Image
                 alt={bank.name}
-                src={`/assets/img/bank/bank_stock.jpg`}
+                // src={`/assets/img/bank/bank_stock.jpg`}
+                src={bank.img_url ? bank.img_url : `/assets/img/bank/bank_stock.jpg`}
                 className={"mx-auto"}
                 width={100}
                 height={100}
