36eed17167deedd1116afb7b4b11cf4dc61f4e3f
Aingaran Thirunadarajah <email@aingaran.com>
2023-02-13 11:28:38 -0500

    Excluded admin route from default Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/Layout";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, ...appProps }) {
+
+  // exclude admin pages from layout
+  if ([`/admin`].includes(appProps.router.pathname))
+    return <Component {...pageProps} />;
+
+  
   return (
     <Layout subNavProps={pageProps.categories}>
       <Component {...pageProps} />
