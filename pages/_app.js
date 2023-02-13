9de89d39a2259950c66a2db61aad8bdd81dee2ba
Aingaran Thirunadarajah <email@aingaran.com>
2023-02-13 13:03:59 -0500

    removed admin / auth from MVD

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,7 @@ import Layout from "@/components/layout/Layout";
 
 export default function App({ Component, pageProps, ...appProps }) {
 
-  // exclude admin pages from layout
-  if ([`/admin`, `/admin/dashboard`].includes(appProps.router.pathname))
-    return <Component {...pageProps} />;
+
 
   
   return (
