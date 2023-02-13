180be96a4a7f8f0bc0b2877cc8a5608eadcdf66f
Aingaran Thirunadarajah <email@aingaran.com>
2023-02-13 12:51:34 -0500

    Updated conditional rendering for admin layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Layout from "@/components/layout/Layout";
 export default function App({ Component, pageProps, ...appProps }) {
 
   // exclude admin pages from layout
-  if ([`/admin`].includes(appProps.router.pathname))
+  if ([`/admin`, `/admin/dashboard`].includes(appProps.router.pathname))
     return <Component {...pageProps} />;
 
   
