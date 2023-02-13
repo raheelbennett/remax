2ae810a9566d466fbfde9a85d57da61e4d40c45e
Aingaran Thirunadarajah <email@aingaran.com>
2023-02-13 13:36:21 -0500

    changed flex to row on mobile view

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -5,8 +5,9 @@ import SearchBar from "../SearchBar";
 const Header = () => {
   return (
     <div className="">
-      <header className="flex fixed container justify-between bg-white text-black items-center opacity-90">
-        <h1>
+      <header className="flex flex-col sm:flex-row first:fixed container justify-between bg-white text-black items-center opacity-90">
+
+        <h1 className="logo">
           <Link href="/">
             <Image alt="logo" src="/assets/img/logo.png" width={200} height={200} priority />
           </Link>
