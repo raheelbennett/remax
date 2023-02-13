22f9f19945a98a1e0894cd5c6babaf495a587a2d
lian-c <lian.chan@telus.com>
2023-02-13 14:04:47 -0800

    change _document to not default to dark

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark" > {/*change to dark here */}
+    <Html lang="en" className="" > {/*change to dark here */}
       <Head />
       <body className="container mx-auto dark:bg-gray-800">
         <Main />
