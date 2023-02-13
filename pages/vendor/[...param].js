c70d6afd5d90a56327e2768a27112a8ab1403253
lian-c <lian.chan@telus.com>
2023-02-13 13:02:56 -0800

    Add dark mode styling for vendors

--- pages/vendor/[...param].js
+++ pages/vendor/[...param].js
@@ -15,30 +15,30 @@ const Vendor = ({ categories, vendor, listings }) => {
   return (
     <section>
       <VendorItem vendor={vendor[0]} />
-      <h2 className="text-xl font-bold mb-4 text-center ">{vendor[0].name}</h2>
-      <div className="text-center">
+      <h2 className="text-xl font-bold mb-4 text-center dark:text-gray-300 ">{vendor[0].name}</h2>
+      <div className="text-center dark:text-gray-300 ">
         If you are shopping here, you should consider these credit cards for maximum benefits!
       </div>
       <div className="card-list grid grid-cols-1 gap-10 m-10">
         {/* only if there's listings */}
         {listings.length > 0 && (
           <div
-            className="p-6 rounded-lg shadow-md bg-amber-100 hover:bg-amber-200 dark:shadow-green-500/40 dark:bg-green-600 dark:hover:bg-green-500"
+            className="p-6 rounded-lg shadow-md bg-amber-100 hover:bg-amber-200  dark:shadow-green-500/40 dark:bg-green-500 dark:hover:bg-green-400"
             key={main.card_id}
           >
             <Link href={`/cards/${main.card_id}`}>
               <div className=" grid grid-cols-2">
                 <CardItem card={main} />
                 <div className="col-start-2 col-end-3 ">
-                  <h3 className="text-xl font-bold text-gray-700">{main.name}</h3>
+                  <h3 className="text-xl font-bold text-gray-700 dark:text-gray-900">{main.name}</h3>
                   <span class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">
                     Highlight
                   </span>
-                  <div className="text-sm text-gray-900">
+                  <div className="text-sm text-gray-900 dark:text-gray-100">
                     Cashback Rate for {vendor[0].name} Eligible Items: {main.reward_rate}%
                   </div>
-                  <div className="text-sm text-gray-700">Annual Fee: ${main.annual_fee}</div>
-                  <div className="text-sm text-gray-700">Interest Rate: {main.interest_rate}%</div>
+                  <div className="text-sm text-gray-700 dark:text-gray-300">Annual Fee: ${main.annual_fee}</div>
+                  <div className="text-sm text-gray-700 dark:text-gray-300">Interest Rate: {main.interest_rate}%</div>
                 </div>
               </div>
             </Link>
